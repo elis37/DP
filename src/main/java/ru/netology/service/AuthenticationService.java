8d2298bf7dcede6fa9fa1c7d42e4befcf3aecfac
frepingod <frepingod@gmail.com>
2022-01-10 21:19:41 +0300

    auth service. login and logout work!!!

--- src/main/java/ru/netology/service/AuthenticationService.java
+++ src/main/java/ru/netology/service/AuthenticationService.java
@@ -1,17 +1,41 @@
 package ru.netology.service;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Service;
 import ru.netology.dto.request.AuthenticationRQ;
 import ru.netology.dto.response.AuthenticationRS;
+import ru.netology.jwt.JwtTokenUtil;
+
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
 
 @Service
+@RequiredArgsConstructor
 public class AuthenticationService {
 
+    private final AuthenticationManager authenticationManager;
+    private final JwtTokenUtil jwtTokenUtil;
+    private final UserService userService;
+
+    private static final Map<String, String> TOKENS = new ConcurrentHashMap<>();
+
     public AuthenticationRS login(AuthenticationRQ authenticationRQ) {
-        return null;
+        final String username = authenticationRQ.getLogin();
+        final String password = authenticationRQ.getPassword();
+        authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(username, password));
+        final UserDetails userDetails = userService.loadUserByUsername(username);
+        final String token = jwtTokenUtil.generateToken(userDetails);
+        TOKENS.put(token, username);
+        return new AuthenticationRS(token);
     }
 
     public void logout(String authToken) {
-
+        final String token = authToken.substring(7);
+        final String username = TOKENS.get(token);
+        System.out.println("User " + username + " logout. JWT is disabled.");
+        TOKENS.remove(token);
     }
 }
