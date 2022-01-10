8ef5ab61d63676566a55f7374ab112fd941456cf
frepingod <frepingod@gmail.com>
2022-01-10 20:58:38 +0300

    model: user and storage-file (not final), user repo

---
+++ src/main/java/ru/netology/model/StorageFile.java
@@ -0,0 +1,26 @@
+package ru.netology.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "files")
+public class StorageFile {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(nullable = false)
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @ManyToOne
+    private User user;
+}
